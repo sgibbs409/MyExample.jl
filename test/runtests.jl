b550f93c02050d08150c809406224b69a97d5de7
Spencer <spencergibbs@gmail.com>
2020-09-11 17:42:16 -0700

    Expanded testing of my_f

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,12 @@
 using MyExample
 using Test
 
-my_f(2,1)
 
+#import MyExample
+#@test my_f(2,1) == 7
 
 @testset "MyExample.jl" begin
-    # Write your tests here.
-
-    f(2,1)
+    # 2x + 3y
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
 end
