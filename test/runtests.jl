82b26d1996c2fc9d63a9fd10f96d14f195655c42
Spencer <2455247+sgibbs409@users.noreply.github.com>
2020-09-21 17:36:21 -0700

    Added coveralls .yml

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,16 @@ using Test
 #import MyExample
 #@test my_f(2,1) == 7
 
+@test_throws TestSetException @testset "MyExample.jl" begin
+    # 2x + 3y
+    @test my_f(2,1) == 7
+    @test my_f(2,3) == 13
+    @test my_f(2,3) == 14
+end
+
 @testset "MyExample.jl" begin
     # 2x + 3y
     @test my_f(2,1) == 7
     @test my_f(2,3) == 13
+    # @test my_f(2,3) == 14
 end
